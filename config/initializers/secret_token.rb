c46bde85ac22559160431387ae84963f68147608
Etienne Vallette d'Osia <dohzya@gmail.com>
2010-09-20 22:10:05 +0200

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CercleDesRolistes::Application.config.secret_token = 'e1df47aae3af648cd903380baa1635dd1d22044d37a7df96a2279f150aea6767adc7774b0305c5243fb9a02f4e2e0fbc7b60c2ad0c8d48c3fb863b0a38da0eb0'
